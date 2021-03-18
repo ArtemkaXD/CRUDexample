e176ccac672533147209b66853f3178e906fcd0b
ArtemkaXD <61840297+ArtemkaXD@users.noreply.github.com>
2021-03-18 14:15:04 +0300

    objects = UserManager()
    add email field in model

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,16 +1,12 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
-from django.db import models, transaction
+from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
-from django.contrib.auth.models import (
-    AbstractBaseUser, PermissionsMixin
-)
-from django.contrib.auth.models import (
-    AbstractBaseUser, PermissionsMixin, BaseUserManager
-)
+from django.contrib.auth.models import UserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser
 from rest_framework.authtoken.models import Token
 
 
@@ -20,35 +16,6 @@ def create_auth_token(sender, instance=None, created=False, **kwargs):
         Token.objects.create(user=instance)
 
 
-class MyUserManager(BaseUserManager):
-
-    def _create_user(self, username, password, **extra_fields):
-        """
-        Creates and saves a User with login and password
-        """
-        if not username:
-            raise ValueError('The given username must be set')
-        try:
-            with transaction.atomic():
-                user = self.model(username=username, **extra_fields)
-                user.set_password(password)
-                user.save(using=self._db)
-                return user
-        except:
-            raise
-
-    def create_user(self, username, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
-        return self._create_user(username, password, **extra_fields)
-
-    def create_superuser(self, username, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        return self._create_user(username, password=password, **extra_fields)
-
-
 class User(AbstractBaseUser, PermissionsMixin):
     """
     An abstract base class implementing a fully featured User model with
@@ -56,13 +23,14 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     """
     username = models.CharField(max_length=32, unique=True)
+    email = models.CharField(max_length=1, null=True)
     first_name = models.CharField(max_length=32, blank=True)
     last_name = models.CharField(max_length=32, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
 
-    objects = MyUserManager()
+    objects = UserManager()
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['first_name', 'last_name']
